67e787697ca6b5c948511742c4089d8bc10dcb5f
DinhoDKK <playsonbr@hotmail.com>
2021-09-22 19:40:40 -0400

    Gerenciando e criando exceções para tratamento de erros com try, except, else e finally

---
+++ aula11.py
@@ -0,0 +1,23 @@
+#TRATAMENTO DE EXCEÇÕES
+lista = [1 , 10]
+
+try:
+    arquivo = open('teste.txt', 'r')
+    texto = arquivo.read()
+    divisao = 10 / 1
+    numero = lista [1]
+    #x = a
+except ZeroDivisionError:
+    print('Não é possível realizar uma divisão por zero')
+except ArithmeticError:
+    print('Houve um erro ao realizar uma operação aritmética')
+except IndexError:
+    print('Erro ao acessar um indice invalido na lista')
+except Exception as ex:
+    print('Erro desconhecido > {}' .format(ex))
+else:
+    print('Executa quando não houver nenhuma exceção')
+finally:
+    print('Sempre executa')
+    print('Fechando arquivo')
+    arquivo.close()
